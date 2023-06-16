3be65061753be09d1b6501c6a0b46f89e476e4cf
Anderson Kardek <kardek.anderson@gmail.com>
2023-06-15 21:30:08 -0400

    feat: starting consume api. Creating Products components

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,9 +1,16 @@
+"use client";
 import { FilterBar } from "@/components/FilterBar";
+import { ProductsList } from "@/components/ProductsList";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function Home() {
+  const client = new QueryClient();
   return (
-    <main>
-      <FilterBar />
-    </main>
+    <QueryClientProvider client={client}>
+      <main>
+        <FilterBar />
+        <ProductsList />
+      </main>
+    </QueryClientProvider>
   );
 }
