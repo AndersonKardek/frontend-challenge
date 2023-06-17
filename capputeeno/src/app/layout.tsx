7327340403e244b302f755ae42c4652f25433348
Anderson Kardek <kardek.anderson@gmail.com>
2023-06-17 09:03:29 -0400

    refactor: extract providers

--- capputeeno/src/app/layout.tsx
+++ capputeeno/src/app/layout.tsx
@@ -1,8 +1,7 @@
 import { Header } from "@/components/Header";
 import "./globals.css";
 import { Saira } from "next/font/google";
-import { Container } from "@/components/Container";
-import { FilterContextProvider } from "@/components/contexts/FilterContext";
+import { DefaultProviders } from "@/components/DefaultProviders";
 
 const saira = Saira({
   weight: ["300", "400", "500", "600", "700"],
@@ -22,11 +21,11 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={saira.className}>
-        <FilterContextProvider>
+        <DefaultProviders>
           <Header />
 
           {children}
-        </FilterContextProvider>
+        </DefaultProviders>
       </body>
     </html>
   );
--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { FilterBar } from "@/components/FilterBar";
 import { ProductsList } from "@/components/ProductsList";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 
 import styles from "./page.module.css";
 import { Container } from "@/components/Container";
@@ -9,13 +9,11 @@ import { Container } from "@/components/Container";
 export default function Home() {
   const client = new QueryClient();
   return (
-    <QueryClientProvider client={client}>
-      <main className={styles.main}>
-        <Container>
-          <FilterBar />
-          <ProductsList />
-        </Container>
-      </main>
-    </QueryClientProvider>
+    <main className={styles.main}>
+      <Container>
+        <FilterBar />
+        <ProductsList />
+      </Container>
+    </main>
   );
 }
